52d6c3f4d0f17883d6d7a38f79595d8dd930f45c
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-06-18 16:38:07 -0300

    adding: media queries

---
+++ components/Card.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+export default function Card({ shirt }) {
+  const {
+    nome,
+    categoria,
+    preco,
+    parcelado,
+    foto_1,
+    artists
+  } = shirt;
+
+  return (
+    <div className={styles.card}>
+
+      <div className={styles.cardTitle}>
+        <h4>{nome}</h4>
+      </div>
+
+      <div className={styles.cardCollection}>
+        <p>{`Coleção: ${categoria} © ${artists ? artists : null}`}</p>
+      </div>
+
+      <div className={styles.cardImage}>
+        <img src={foto_1} alt={nome} />
+      </div>
+
+      <div className={styles.cardPrice}>
+        <strong>
+          {
+            `R$ ${Number(preco.replace(',', '.')) // with "."
+              .toFixed(2)
+              .replace('.', ',')}` // with ","
+          }
+        </strong>
+        <span className={styles.parcelado}>OU {parcelado}</span>
+      </div>
+
+    </div>
+  );
+};
